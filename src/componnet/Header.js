b51463f1dcf8c6a107b67a6e68d7d7c433c11468
debreroha <debreroha12@gmail.com>
2023-10-20 19:10:59 +0300

    first and last commit once

---
+++ src/componnet/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+import Button from "./Button";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h2> {title}</h2>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// css in js
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "yellow",
+// };
+
+export default Header;
